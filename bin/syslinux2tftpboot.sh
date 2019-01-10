913d2692f67d4248bc2337ce5c13e05117262f36
github.com_rcpao-enmotus_pxelinux_rc.pixi-efi-vm.rs.paonet.org <pixi-efi-vm20181220.rcpao_en@xoxy.net>
2019-01-10 08:47:17 -0800

    Use /tmp//tmp/tmp.oIQRM9Sg9I instead of current directory to get and extract the syslinux binaries.

--- bin/syslinux2tftpboot.sh
+++ bin/syslinux2tftpboot.sh
@@ -1,10 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 # https://www.syslinux.org/wiki/index.php?title=PXELINUX#Custom_Menu_Example_with_sub-menus
 
+
+MKTMPDIR=`mktemp -d`
+pushd $MKTMPDIR
+
+
 # https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.xz
 # https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/Testing/6.04/syslinux-6.04-pre1.tar.xz
-# pwd=/home/rcpao/Download/syslinux-6.04-pre1
+SYSLINUXURL=https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/Testing/6.04/syslinux-6.04-pre1.tar.xz
+SYSLINUXBASE=`basename syslinux-6.04-pre1 .tar.xz`
+
+[ ! -f $SYSLINUXBASE.tar.xz ] && wget $SYSLINUXURL
+ls $SYSLINUXBASE.tar.xz
+tar xf $SYSLINUXBASE.tar.xz
+
+cd $SYSLINUXBASE
+
 
 TFTPBOOTDIR=../tftpboot 
 BIOSDIR=$TFTPBOOTDIR
@@ -13,6 +26,7 @@ BIOSDIR=$TFTPBOOTDIR
 # [ ! -d $EFI32DIR ] && mkdir -p $EFI32DIR
 EFI64DIR=$TFTPBOOTDIR/efi64
 [ ! -d $EFI64DIR ] && mkdir -p $EFI64DIR
+
 
 # Legacy BIOS
 cp ./bios/core/pxelinux.0 $BIOSDIR
@@ -32,3 +46,16 @@ cp ./efi64/efi/syslinux.efi $EFI64DIR
 cp ./efi64/com32/elflink/ldlinux/ldlinux.e64 $EFI64DIR
 cp ./efi64/com32/menu/menu.c32 $EFI64DIR
 cp ./efi64/com32/libutil/libutil.c32 $EFI64DIR
+
+
+popd
+
+
+ls -R $MKTMPDIR/tftpboot
+
+echo "$0 Do the following manually:"
+echo "$0 cp -r $MKTMPDIR/tftpboot/* /var/lib/tftpboot/"
+echo "$0 rm -r $MKTMPDIR"
+
+
+exit
